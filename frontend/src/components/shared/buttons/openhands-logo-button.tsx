435fdab5c065be4ce5df29856fa6308f5e74563e
ftboy <futao10@huawei.com>
2026-02-05 19:55:52 +0800

    修改Home按钮

--- frontend/src/components/shared/buttons/openhands-logo-button.tsx
+++ frontend/src/components/shared/buttons/openhands-logo-button.tsx
@@ -1,19 +1,19 @@
 import { NavLink } from "react-router";
 import { useTranslation } from "react-i18next";
-import OpenHandsLogo from "#/assets/branding/openhands-logo.svg?react";
+import HomeIcon from "#/assets/branding/home-icon.svg?react";
 import { I18nKey } from "#/i18n/declaration";
 import { StyledTooltip } from "#/components/shared/buttons/styled-tooltip";
 
 export function OpenHandsLogoButton() {
   const { t } = useTranslation();
 
-  const tooltipText = t(I18nKey.BRANDING$OPENHANDS);
-  const ariaLabel = t(I18nKey.BRANDING$OPENHANDS_LOGO);
+  const tooltipText = t(I18nKey.BRANDING$HOME);
+  const ariaLabel = t(I18nKey.BRANDING$HOME_BUTTON);
 
   return (
     <StyledTooltip content={tooltipText}>
       <NavLink to="/" aria-label={ariaLabel}>
-        <OpenHandsLogo width={46} height={30} />
+        <HomeIcon width={46} height={30} />
       </NavLink>
     </StyledTooltip>
   );
